6bc1d7657749281cb76c172aba12e6e1ecd25566
IMNOTIKE <simone.oliva.work@gmail.com>
2023-01-19 19:13:41 +0000

    changed ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import {
+  AppShell,
+  Header,
+  Group,
+  Table,
+  Title,
+  Button,
+  MantineProvider,
+  Text,
+  Anchor,
+  Center,
+  Skeleton
+} from '@mantine/core';
+import {
+  IconBrandSpotify,
+  IconLogin,
+  IconLogout,
+  IconRefresh,
+  IconAddressBook
+} from '@tabler/icons';
+
 import axios from 'axios';
 
 function App() {
 
   const [token, setToken] = useState("")
   const [songs, setSongs] = useState([])
-
+  const [limited, setLimited] = useState(false)
   const scopes = 'user-library-read playlist-read-private playlist-modify-private playlist-modify-public'
 
   useEffect(() => {
@@ -31,45 +52,116 @@ function App() {
   }
 
   const getSongs = async () => {
-    const { data } = await axios.get("https://api.spotify.com/v1/me/tracks", {
+    await axios.get("https://api.spotify.com/v1/me/tracks?limit=15", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(res => {
+      setSongs(res.data.items)
+    }).catch(
+      err => {
+        if ( err.response.status === 429 ){
+          console.log("Limited rate")
+          console.log(err.response.status)
+          setLimited(true)
+        }else{
+          console.log(err.response.status)
+        }
+      }
+    )
+
+    
+  }
+
+  const getArtistGenre = async (artistId) => {
+    const { data } = await axios.get(`https://api.spotify.com/v1/artists/${artistId}`, {
       headers: {
         Authorization: `Bearer ${token}`
       }
     })
-
-    setSongs(data.items)
+    return data.genres.join(", ")
   }
 
-  const renderArtists = () => {
-    console.log("render is triggered")
-    return songs.map((song => (
-      <div key={song.track.id}>
-        {song.track.name}
-      </div>
-    ))
+  const ths = (
+    <tr>
+      <th>Title</th>
+      <th>Artits</th>
+      {/* <th>Genres</th> */}
+    </tr>
+  )
 
+  const rows = songs.map((song) => (
+    <tr key={song.track.id}>
+      <td>{song.track.name}</td>
+      <td>{song.track.artists.join(", ")}</td>
+      {/* <td>{getArtistGenre(song.track.artists[0].id)}</td> */}
+    </tr>
+
+  )
+  )
+
+  const renderSongs = () => {
+    console.log("render is triggered")
+    return (
+      <Center>
+        <Table striped highlightOnHover withBorder p={'lg'}>
+          <thead>{ths}</thead>
+          <tbody>{rows}</tbody>
+        </Table>
+      </Center>
     )
+
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Spotify React</h1>
-        {!token ?
-          <a href={`${process.env.REACT_APP_AUTH_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=https://genrify.github.io/&response_type=${process.env.REACT_APP_RESPONSE_TYPE}&scope=${scopes}`}>Login
-            to Spotify</a>
-          : <button onClick={logout}>Logout</button>}
-          <div>
-        {token ?
-          <button onClick={getSongs}>Get songs list</button>
 
-          : <p>Log in please</p>
+    <MantineProvider theme={{ fontFamily: 'Open Sans', colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
+      <AppShell
+        padding="md"
+        header={<Header height={70} p="xs">{
+          <Group position='apart'>
+            <Button leftIcon={<IconRefresh />} variant='subtle' onClick={getSongs()} />
+            <Center>
+              <Title pl={'md'} style={{
+                fontFamily: 'Pacifico'
+              }}>
+                Genrify
+              </Title>
+            </Center>
+            <Center>
+              {!token ?
+                <Button pl={'md'} color={'green'} leftIcon={<IconLogin />} component='a' href={`${process.env.REACT_APP_AUTH_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=http://localhost:3000&response_type=${process.env.REACT_APP_RESPONSE_TYPE}&scope=${scopes}`}>Login
+                  to Spotify</Button>
+                : <Button pr={'md'} color={'grape'} leftIcon={<IconLogout />} onClick={logout}>Log out</Button>}
+            </Center>
+          </Group>
+        }</Header>}
+        footer={
+          <Group position='center' p={"md"}>
+            <IconBrandSpotify />
+            <Text>
+              ISIALP x Spotify
+            </Text>
+            <IconAddressBook />
+            <Anchor href='https://isialp.github.io' color={'dimmed'} >
+              check us out
+            </Anchor>
+          </Group>
         }
-        {renderArtists()}
-      </div>
-      </header>
-      
-    </div>
+        styles={(theme) => ({
+          main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+        })}
+      >
+        <Center>
+
+          <Skeleton visible={token}>
+            {renderSongs()}
+          </Skeleton>
+
+        </Center>
+      </AppShell>
+    </MantineProvider>
+
   );
 }
 
